3353461b7f087ee9126ecd6e683680fe874998ce
syoinGit <sasasyuga@yahoo.co.jp>
2024-09-14 10:49:33 +0900

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
 
   public static void main(String[] args) {
-    String answer = org.example.Input.UserInput();
+    String answer = Input.UserInput();
     System.out.println("計算結果: " + answer);
   }
 }
